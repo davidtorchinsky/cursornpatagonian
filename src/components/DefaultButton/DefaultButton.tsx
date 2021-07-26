05f7eff44ca0a5493b9aa515f7087f50cc1d5711
David <davidtorhcinsky@gmail.com>
2021-07-26 20:38:25 -0300

    integrate husky and text y button component

--- src/components/DefaultButton/DefaultButton.tsx
+++ src/components/DefaultButton/DefaultButton.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity, ViewStyle} from 'react-native';
 
 import styles from './styles';
 
 interface Props {
   onPress: () => void;
+  textSize: number;
   title: String;
+  additionalStyle?: ViewStyle;
 }
 
-const DefaultButton = ({onPress, title}: Props) => {
+const DefaultButton = ({additionalStyle, onPress, textSize, title}: Props) => {
   return (
-    <TouchableOpacity style={styles.mainContainer} onPress={onPress}>
-      <Text>{title}</Text>
+    <TouchableOpacity style={[styles.mainContainer, additionalStyle]} onPress={onPress}>
+      <Text style={{fontSize: textSize}}>{title}</Text>
     </TouchableOpacity>
   );
+};
+DefaultButton.defaultProp = {
+  additionalStyle: {},
+  textSize: 14,
 };
 
 export default DefaultButton;
